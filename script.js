88683c649a5ceb4f5091f62604ad9f427ed831fa
Iris-Ghislaine <ganzairis03@gmail.com>
2025-09-13 18:12:58 +0200

    updated navigation bar with darkmode icon and added responsiveness on navigation bar

---
+++ script.js
@@ -0,0 +1,10 @@
+// Mobile menu toggle
+function toggleMobileMenu() {
+    const mobileMenu = document.getElementById('mobileMenu');
+    if (mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.remove('hidden');
+    } else {
+        mobileMenu.classList.add('hidden');
+    }
+}
+

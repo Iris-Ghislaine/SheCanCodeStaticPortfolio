2e3c7b72cf2b844df38c05c4c6e227398e1a0e46
Iris-Ghislaine <ganzairis03@gmail.com>
2025-09-14 23:44:43 +0200

    added a darkmode theme

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ function toggleMobileMenu() {
     }
 }
 
+// Dark mode toggle
+function toggleTheme() {
+    const html = document.documentElement;
+    const isDark = html.classList.contains('dark');
+    const body = document.getElementsByTagName('body')
+    
+    if (isDark) {
+        html.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+    } else {
+        html.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Check for saved theme or default to light mode
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+        document.documentElement.classList.add('dark');
+    }
+})
